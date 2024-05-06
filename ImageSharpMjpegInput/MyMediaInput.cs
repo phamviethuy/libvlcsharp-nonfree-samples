a30a02f99737e23efddc8e880640ffdf308dba3e
Huy <phamviethuy371@gmail.com>
2024-05-07 00:45:30 +0700

    update

---
+++ ImageSharpMjpegInput/MyMediaInput.cs
@@ -0,0 +1,85 @@
+﻿using Basler.Pylon;
+using LibVLCSharp.Shared;
+using System;
+using System.Collections.Concurrent;
+
+namespace ImageSharpMjpegInput
+{
+    internal class MyMediaInput : MediaInput
+    {
+        private readonly Camera camera = new();
+        private readonly ConcurrentQueue<IntPtr> frames = new();
+        private readonly ManualResetEvent ManualResetEvent = new(false);
+
+        public MyMediaInput()
+        {
+            camera.CameraOpened += Configuration.AcquireContinuous;
+            camera.Open();
+            camera.Parameters[PLCamera.PixelFormat].SetValue("BGR8Packed");
+            camera.Parameters[PLCamera.AcquisitionFrameRateEnable].SetValue(true);
+            camera.Parameters[PLCamera.AcquisitionFrameRateAbs].SetValue(30.0);
+            camera.StreamGrabber.Start();
+            StartCapture();
+        }
+
+        private void StartCapture()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    using var grabResult = camera.StreamGrabber.RetrieveResult(1000, TimeoutHandling.Return);
+                    if (grabResult == null || !grabResult.IsValid)
+                    {
+                        Thread.Sleep(1);
+                        continue;
+                    }
+                    frames.Enqueue(grabResult.PixelDataPointer);
+                    ManualResetEvent.Set();
+                    Thread.Sleep(1);
+                }
+            });
+        }
+
+        public override void Close()
+        {
+            camera.StreamGrabber.Stop();
+        }
+
+        public override bool Open(out ulong size)
+        {
+            size = ulong.MaxValue;
+
+            return true;
+        }
+
+        public unsafe override int Read(IntPtr buf, uint len)
+        {
+            ManualResetEvent.WaitOne();
+            var isOk = frames.TryDequeue(out var intPt);
+            if (!isOk)
+            {
+                return (int)(len);
+            }
+            var capturedFrame = new Span<byte>(intPt.ToPointer(), (int)len);
+            var buffer = (capturedFrame.Length > len) ? capturedFrame[..(int)len] : capturedFrame;
+            var outputBuffer = new Span<byte>(buf.ToPointer(), (int)len);
+
+            // Copy captured frame to buffer
+            if (capturedFrame.Length <= len)
+            {
+                System.Runtime.InteropServices.Marshal.Copy(capturedFrame, 0, buf, capturedFrame.Length);
+                ManualResetEvent.Reset();
+                return capturedFrame.Length;
+            }
+
+            return capturedFrame.Length;
+        }
+
+        public override bool Seek(ulong offset)
+        {
+            return false;
+        }
+
+    }
+}
--- ImageSharpMjpegInput/Program.cs
+++ ImageSharpMjpegInput/Program.cs
@@ -3,21 +3,31 @@
 // After payment, the file is considered yours and no copyright notice is required (though it would be appreciated).
 // The file is provided as-is without any guarantee or support, and you still need to comply to the licenses of the dependencies of this file.
 
+using ImageSharpMjpegInput;
 using LibVLCSharp.Shared;
 using System.IO.Pipelines;
-using ImageSharpMjpegInput;
 
 Core.Initialize();
 
-using var libVLC = new LibVLC("--demux=mjpeg");
+using var libVLC = new LibVLC();
 var pipe = new Pipe();
-using var mediaInput = new PipeMediaInput(pipe.Reader);
+using var mediaInput = new MyMediaInput();
 using var media = new Media(libVLC, mediaInput);
+
+// Set video parameters
+media.AddOption(":demux=rawvideo");
+media.AddOption(":rawvid-width=" + 1024);
+media.AddOption(":rawvid-height=" + 1040);
+media.AddOption(":rawvid-format=RGB");
+media.AddOption(":rawvid-fps=" + 30);
+media.AddOption(":rawvid-chroma=" + "RV24");
+
 using var mp = new MediaPlayer(media);
 
-var form = new Form();
-
-form.ClientSize = new Size(1280, 720);
+var form = new Form
+{
+    ClientSize = new Size(1280, 720)
+};
 
 form.Load += (s, e) =>
 {
@@ -29,13 +39,12 @@ form.Show();
 
 var cancellationTokenSource = new CancellationTokenSource();
 
-var producerTask = Task.Run(() =>
-{
-
-    //new ProducerPylon(pipe.Writer, cancellationTokenSource.Token);
-    //new ProducerFlashCap(pipe.Writer, cancellationTokenSource.Token);
-    new ProducerCV(pipe.Writer, cancellationTokenSource.Token);
-});
+//var producerTask = Task.Run(() =>
+//{
+//    new ProducerPylon(pipe.Writer, cancellationTokenSource.Token);
+//    //new ProducerFlashCap(pipe.Writer, cancellationTokenSource.Token);
+//    // ProducerCV.Run(pipe.Writer, cancellationTokenSource.Token);
+//});
 
 form.FormClosing += (s, e) =>
 {
